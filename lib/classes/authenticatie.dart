c62af79ff97c5a7a7e31cd7f531f04318b5a403c
jesse <jonesvandevelde@gmail.com>
2026-03-11 10:44:39 +0100

    use different url to send request (for cross origin requests)

--- lib/classes/authenticatie.dart
+++ lib/classes/authenticatie.dart
@@ -2,9 +2,9 @@ import 'package:dio/dio.dart';
 
 class Authenticatie {
   final Dio _dio = Dio(BaseOptions(
-    baseUrl: 'http://127.0.0.1:8000',
+    baseUrl: 'http://10.0.2.2:8000',
     connectTimeout: const Duration(seconds: 5),
-    receiveTimeout: const Duration(seconds: 3),
+    receiveTimeout: const Duration(seconds: 30),
   ));
 
   Future<Response?> login(String email, String password) async {
