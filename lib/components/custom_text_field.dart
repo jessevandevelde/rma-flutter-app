aa13f912f66b9851cce3389cae7d2ff3eff0f840
jesse <jonesvandevelde@gmail.com>
2026-03-24 13:16:04 +0100

    made project component base so i dont get slimed out by jarrin

---
+++ lib/components/custom_text_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String hint;
+  final int maxLines;
+
+  const CustomTextField({
+    super.key,
+    required this.hint,
+    this.maxLines = 1,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      maxLines: maxLines,
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: TextStyle(color: Colors.grey[400]),
+        filled: true,
+        fillColor: Colors.white,
+        contentPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide(color: Colors.grey[200]!),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide(color: Colors.blue[200]!),
+        ),
+      ),
+    );
+  }
+}
