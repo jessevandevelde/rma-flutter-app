967816a2d9c2adee5dd0fc9d7218e7cf4d34c8c7
180931371 <585100@edu.rocmn.nl>
2026-04-08 14:23:40 +0200

    bug fix

--- lib/classes/authenticatie.dart
+++ lib/classes/authenticatie.dart
@@ -7,8 +7,11 @@ class Authenticatie {
   late final Dio _dio;
 
   Authenticatie() {
+    // Dynamische baseUrl op basis van platform
     String baseUrl = 'http://127.0.0.1:8000';
+
     if (!kIsWeb && Platform.isAndroid) {
+      // 10.0.2.2 is voor Android Emulators om de host machine te bereiken
       baseUrl = 'http://10.0.2.2:8000';
     }
 
@@ -19,6 +22,17 @@ class Authenticatie {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
+      },
+    ));
+
+    _dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        final prefs = await SharedPreferences.getInstance();
+        final token = prefs.getString('auth_token');
+        if (token != null) {
+          options.headers['Authorization'] = 'Bearer $token';
+        }
+        return handler.next(options);
       },
     ));
   }
@@ -35,7 +49,15 @@ class Authenticatie {
 
       if (response.statusCode == 200 || response.statusCode == 201) {
         final data = response.data;
+        final token = data['access_token'] ?? data['token'];
         final prefs = await SharedPreferences.getInstance();
+
+        dynamic userTypeId;
+        if (data['user'] != null) {
+          userTypeId = data['user']['user_type_id'];
+        } else if (data['user_type_id'] != null) {
+          userTypeId = data['user_type_id'];
+        }
 
         // Save User ID
         final userId = data['user']?['id'];
@@ -43,16 +65,40 @@ class Authenticatie {
           await prefs.setInt('user_id', userId);
         }
 
-        // Save Auth Token (supporting various common keys like 'token' or 'access_token')
-        final token = data['access_token'] ?? data['token'];
+        // Save Auth Token
         if (token != null) {
           await prefs.setString('auth_token', token.toString());
+          if (userTypeId != null) {
+            await prefs.setInt('user_type_id', int.parse(userTypeId.toString()));
+          }
         }
       }
       return response;
     } on DioException catch (e) {
       return e.response;
     }
+  }
+
+  Future<String?> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('auth_token');
+  }
+
+  Future<int?> getUserType() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('user_type_id');
+  }
+
+  Future<int?> getUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt('user_id');
+  }
+
+  Future<void> logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('auth_token');
+    await prefs.remove('user_type_id');
+    await prefs.remove('user_id');
   }
 
   Future<Response?> forgotPassword(String email) async {
@@ -67,21 +113,5 @@ class Authenticatie {
     } on DioException catch (e) {
       return e.response;
     }
-  }
-
-  Future<int?> getUserId() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getInt('user_id');
-  }
-
-  Future<String?> getToken() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString('auth_token');
-  }
-
-  Future<void> logout() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.remove('user_id');
-    await prefs.remove('auth_token');
   }
 }
